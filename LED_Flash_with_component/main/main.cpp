000cf542badb64bbd7c9e4824a06d8d9731b6d3a
siriwan2004 <puttharasusiriwan@gmail.com>
2025-09-18 23:17:53 +0700

    Challenge1

--- LED_Flash_with_component/main/main.cpp
+++ LED_Flash_with_component/main/main.cpp
@@ -3,25 +3,42 @@
 #include "freertos/task.h"
 #include "LED.h"
 
-LED led1(16); 
-LED led2(17); 
-LED led3(5); 
-LED led4(18); 
-LED led5(19); 
-LED led6(21); 
-LED led7(22); 
-LED led8(23); 
+LED led1(GPIO_NUM_16); 
+LED led2(GPIO_NUM_17); 
+LED led3(GPIO_NUM_5); 
+LED led4(GPIO_NUM_18); 
+LED led5(GPIO_NUM_19); 
+LED led6(GPIO_NUM_21); 
+LED led7(GPIO_NUM_22); 
+LED led8(GPIO_NUM_23); 
 
-LED leds[] = {led1, led2, led3, led4, led5, led6, led7, led8};
+LED* leds[] = {&led1, &led2, &led3, &led4, &led5, &led6, &led7, &led8};
+const int num_leds = sizeof(leds) / sizeof(leds[0]);
+
+// ดับทั้งหมด
+void allOff() {
+    for (int i = 0; i < num_leds; i++) leds[i]->OFF();
+}
+
+// พิมพ์ pattern ลง Serial Monitor
+void printPattern(int on1, int on2 = -1) {
+    for (int i = 0; i < num_leds; i++) {
+        if (i == on1 || i == on2) printf("*");
+        else printf(".");
+    }
+    printf("\n");
+}
 
 extern "C" void app_main(void)
 {
-    int i = 0;
-    while(1)
-    {        
-        leds[i].ON();
-        vTaskDelay(100/portTICK_PERIOD_MS);
-        leds[i].OFF();
-        if(i++ >= 7) i = 0;
+    while (true) {
+
+        for (int i = 0; i < num_leds; i++) {
+            allOff();
+            leds[i]->ON();
+            printPattern(i);
+            vTaskDelay(pdMS_TO_TICKS(200));
+        }
+
     }
 }
